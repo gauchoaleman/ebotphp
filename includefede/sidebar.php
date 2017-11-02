0b1808685762c2b25e2e321cb24fd97e92c6269e
unknown <fedetrabajo13@gmail.com>
2017-11-02 19:56:21 -0300

    subiendo includefede e indexfede

---
+++ includefede/sidebar.php
@@ -0,0 +1,38 @@
+<div class="row">
+    <div class="col-md-10">
+
+    <div id="carousel-1" class="carousel slide" data-ride="carousel">
+          <ol class="carousel-indicators">
+            <li data-target="#carousel_1" data-slide-to="0" class="active"></li>
+            <li data-target="#carousel_1" data-slide-to="1"></li>
+            <li data-target="#carousel_1" data-slide-to="2"></li>
+          </ol>
+          <div class="carousel-inner" role="listbox">
+            <div class="item active">
+              <img src="img\zinniadoble.png" class="img-responsive" alt="">
+              <div class="carousel-caption d-none d-md-block">
+                <h3>Oferta del %50</h3>
+                <p>disponible solo hasta el 20 de noviembre</p>
+              </div>
+            </div>
+
+            <div class="item">
+              <img src="img\mezclarocallia.png" class="img-responsive" alt="">
+              <div class="carousel-caption d-none d-md-block">
+                <h3>Oferta del %75</h3>
+                <p>disponible solo hasta el 10 de noviembre</p>
+              </div>
+            </div>
+
+            <div class="item">
+              <img src="img\lupinoazul.png" class="img-responsive" alt="">
+              <div class="carousel-caption d-none d-md-block">
+                <h3>Oferta del %20</h3>
+                <p>disponible solo hasta el 25 de noviembre</p>
+              </div>
+            </div>
+
+          </div>
+        </div>
+
+</div>
---
+++ indexfede.php
@@ -0,0 +1,11 @@
+<?php require "includefede/header.php"; ?>
+
+<?php require "includefede/sidebar.php"; ?>
+
+    <!-- Librería jQuery requerida por los plugins de JavaScript -->
+    <script src="http://code.jquery.com/jquery.js"></script>
+
+    <!-- Todos los plugins JavaScript de Bootstrap (también puedes
+         incluir archivos JavaScript individuales de los únicos
+         plugins que utilices) -->
+    <script src="js/bootstrap.min.js"></script>
