9053bb9c05b567e356fae730ec0c72265d99acbb
unknown <fedetrabajo13@gmail.com>
2017-11-02 21:12:01 -0300

    sidebar funcionando

--- includefede/sidebar.php
+++ includefede/sidebar.php
@@ -1,38 +1,18 @@
 <div class="row">
-    <div class="col-md-12">
+    <div class="col-md-8">
 
-    <div id="carousel-1" class="carousel slide" data-ride="carousel">
-          <ol class="carousel-indicators">
-            <li data-target="#carousel_1" data-slide-to="0" class="active"></li>
-            <li data-target="#carousel_1" data-slide-to="1"></li>
-            <li data-target="#carousel_1" data-slide-to="2"></li>
-          </ol>
-          <div class="carousel-inner" role="listbox">
-            <div class="item active">
-              <img src="img\1.png" class="img-responsive" alt="">
-              <div class="carousel-caption d-none d-md-block">
-                <h3>Oferta del %50</h3>
-                <p>disponible solo hasta el 20 de noviembre</p>
-              </div>
-            </div>
-
-            <div class="item">
-              <img src="img\2.png" class="img-responsive" alt="">
-              <div class="carousel-caption d-none d-md-block">
-                <h3>Oferta del %75</h3>
-                <p>disponible solo hasta el 10 de noviembre</p>
-              </div>
-            </div>
-
-            <div class="item">
-              <img src="img\3.png" class="img-responsive" alt="">
-              <div class="carousel-caption d-none d-md-block">
-                <h3>Oferta del %20</h3>
-                <p>disponible solo hasta el 25 de noviembre</p>
-              </div>
-            </div>
-
+      <div id="carouselExampleSlidesOnly" class="carousel slide" data-ride="carousel">
+        <div class="carousel-inner">
+          <div class="carousel-item active">
+            <img class="d-block w-100" src="img/1.png" alt="First slide">
+          </div>
+          <div class="carousel-item">
+            <img class="d-block w-100" src="img/2.png" alt="Second slide">
+          </div>
+          <div class="carousel-item">
+            <img class="d-block w-100" src="img/3.png" alt="Third slide">
           </div>
         </div>
+      </div>
 
 </div>
