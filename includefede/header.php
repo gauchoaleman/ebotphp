74efabdd7ec051b25eeca9d0d0544b5bf28fb3f6
unknown <fedetrabajo13@gmail.com>
2017-11-02 21:01:28 -0300

    Nuevo menu andando

--- includefede/header.php
+++ includefede/header.php
@@ -15,48 +15,56 @@
         <img class="img-responsive" src="img\Logobotandino.png" height="109" width="150" alt="Responsive image">
       </div>
       <div class="col-md-4">
+      </br>
         <form class="form-inline my-2 my-lg-0">
-        <br><br>
-        <br><br>
           <input class="form-control mr-sm-2" type="search" placeholder="Buscar producto" aria-label="Buscar producto">
           <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Buscar</button>
         </form>
       </div>
       <div class="col-md-4">
-        &nbsp;
       </div>
     </div>
     <div class="row">
       <div class="col-md-12">
 
-      <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="#">Navbar</a>
+      <nav class="navbar navbar-expand-lg navbar-light" style="background-color: #03c112;">
+  <a class="navbar-brand" href="#"></a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
 
   <div class="collapse navbar-collapse" id="navbarSupportedContent">
     <ul class="navbar-nav mr-auto">
-      <li class="nav-item active">
-        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Link</a>
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Jardinería
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item" href="#">Semillas</a>
+        </div>
       </li>
       <li class="nav-item dropdown">
         <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          Dropdown
+          Gourmet
         </a>
         <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-          <a class="dropdown-item" href="#">Action</a>
-          <a class="dropdown-item" href="#">Another action</a>
-          <div class="dropdown-divider"></div>
-          <a class="dropdown-item" href="#">Something else here</a>
+          <a class="dropdown-item" href="#">Té</a>
         </div>
       </li>
-      <li class="nav-item">
-        <a class="nav-link disabled" href="#">Disabled</a>
+
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Cosmética
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item" href="#">Jabón vegetal</a>
+          <a class="dropdown-item" href="#">Mascarillas</a>
+          <a class="dropdown-item" href="#">Sales de baño</a>
+          <a class="dropdown-item" href="#">Aceite esencial</a>
+        </div>
+
       </li>
+
     </ul>
   </div>
 </nav>
